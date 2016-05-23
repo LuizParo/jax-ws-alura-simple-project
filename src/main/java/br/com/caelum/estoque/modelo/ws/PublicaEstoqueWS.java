56bf0fd12dda0ea32300ecf0a99800daa46931d7
Luiz Guilherme Paro <lg_paro@hotmail.com>
2016-05-23 20:38:10 -0300

    First endpoint published

---
+++ src/main/java/br/com/caelum/estoque/modelo/ws/PublicaEstoqueWS.java
@@ -0,0 +1,13 @@
+package br.com.caelum.estoque.modelo.ws;
+
+import javax.xml.ws.Endpoint;
+
+public class PublicaEstoqueWS {
+    public static void main(String[] args) {
+        EstoqueWS implementacao = new EstoqueWS();
+        String url = "http://localhost:8080/estoquews";
+        
+        System.out.println("EstoqueWS rodando: " + url);
+        Endpoint.publish(url, implementacao);
+    }
+}
