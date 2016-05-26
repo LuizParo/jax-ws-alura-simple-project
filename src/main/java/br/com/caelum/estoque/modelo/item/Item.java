1a2bb39c737c29db4eeb208d08f6ee154a19ab9d
Luiz Guilherme Paro <lg_paro@hotmail.com>
2016-05-26 19:42:39 -0300

    Fixing a bug on Item class anntotations

--- src/main/java/br/com/caelum/estoque/modelo/item/Item.java
+++ src/main/java/br/com/caelum/estoque/modelo/item/Item.java
@@ -1,7 +1,12 @@
 package br.com.caelum.estoque.modelo.item;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Item {
 
     @XmlElement(required = true)
